4fe4424e1ecde4f124c7b955656b5343c4ea12a6
root <root@chat.dev0.spark.net>
2013-08-13 21:07:15 -0400

    use bosh for http-bind

--- src/spark_simple_xmpp_client.erl
+++ src/spark_simple_xmpp_client.erl
@@ -1,16 +1,18 @@
 -module(spark_simple_xmpp_client).
 -export([connect/4,
-	     init_session/2,
+		 send_presence/2
+	     try_authenticate/3,
+	     init_bosh_session/5,
 	     disconnect/1]).
 	     
 -include_lib(exmpp-0.9.9/include/exmpp_jid.hrl).
 -include_lib(exmpp-0.9.9/include/exmpp_client.hrl).
 	     
 connect(UserName, Password, Host, Port)->
-  Session = exmpp_session:start(),
+  Session = exmpp_session:start({1,0}), 
   Jid = spark_xmpp_user:make_jid(UserName),
   case init_session(Session) of
-  	 loggedin -> {ok, {Session, Jid}};
+  	 logged_in -> {ok, {Session, Jid}};
   	 not_authorized -> {ok, {Jid, not_authorized}}
   end.
 
@@ -18,14 +20,24 @@ disconnect(Session) ->
   catch(exmpp_session:stop(Session)),
   ok.
 
-init_session(Session)->
-  try 
-     {ok, _} = exmpp_session(Session),
-     {ok, loggedin}; 
+try_authenticate(Session, Jid, Password)->
+  try exmpp_session:auth_basic(Session, Jid, Password) of
+     {ok, _} -> {ok, logged_in} 
   catch
      throw:(auth_error, 'not-authorized') ->
      	{error, not_authorized}     
-  end
+  end.
+  
+init_bosh_session(Session, 
+				  HttpBind,
+				  Host,
+				  Port,
+				  Jid)-> 
+  {ok, StreamId, _Features} = 
+  		exmpp_session:connect_BOSH(Session, 
+        HttpBind, Host,[]),
+  {ok, StreamId}.
+  
 
 send_presence(_, not_authorized)-> {ok, not_authorized};
 send_presence(Session, loggedin) ->
